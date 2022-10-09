c3ee4ca292c03d5f1c3b0efd8114f09d16ef90e5
Sven Korset <sven.korset@indie-software.com>
2022-10-09 16:05:27 +0200

    Package dependency update

--- Package.swift
+++ Package.swift
@@ -21,9 +21,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "INNavigation",
-            dependencies: []),
+            dependencies: ["INCommons"]),
         .testTarget(
             name: "INNavigationTests",
-            dependencies: ["INNavigation"]),
+            dependencies: ["INNavigation","INCommons"]),
     ]
 )
